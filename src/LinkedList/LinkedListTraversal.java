dba3cf50d5c8fad92413981160de571ae4bf202c
ninomeister <rajesh.thum@gmail.com>
2015-01-05 11:05:39 -0800

    Adding Initial LinkedList

---
+++ src/LinkedList/LinkedListTraversal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package LinkedList;
+
+/**
+ * @author rthummal
+ *
+ */
+public class LinkedListTraversal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
