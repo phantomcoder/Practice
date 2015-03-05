7f0efaf0842f2b71963b530afa969ef162ecd1ec
ninomeister <rajesh.thum@gmail.com>
2015-03-05 10:48:07 -0800

    l
    l

---
+++ src/Arrays/LongestSubsetWithDifferenceZeroOrOne.java
@@ -0,0 +1,48 @@
+package Arrays;
+
+public class LongestSubsetWithDifferenceZeroOrOne {
+
+	public static void main(String[] args) 
+	{
+	
+		   int[] input = {2,6,7,9,1,0,1,0,2,3,6};
+		    
+		    printSubset(input);
+		    }
+	
+
+    private static void printSubset(int[] a)
+    {
+    	int size = a.length;
+    	int sequenceStart = 0, sequenceEnd = 0;
+    	int finalSequenceStart = 0, finalSequenceEnd = -1;
+    	int min = a[0];
+    	
+    	for(int j = 0;j< size; j++)
+    	{
+    		if((a[j]-min <= 1) && (a[j] - min >= -1))
+    		{
+    			sequenceEnd = j;
+    			if(a[j] < min)
+    				min = a[j];
+    			if((finalSequenceEnd-finalSequenceStart)< (sequenceEnd-sequenceStart))
+    			{
+    				finalSequenceEnd = sequenceEnd;
+    				finalSequenceStart = sequenceStart;
+    			}
+    			
+    		}
+    		else
+    		{
+    			min = a[j];
+    			//max = a[j];
+    			sequenceStart = j;
+    			sequenceEnd = j;
+    		}
+    	}
+    	
+    	System.out.println("Sequence Start is: "+finalSequenceStart+" Sequence End is:" +finalSequenceEnd);
+    	
+    }
+
+}
---
+++ src/InterviewQuestions/ReverseSentenceInConstantSpace.java
@@ -0,0 +1,19 @@
+package InterviewQuestions;
+
+public class ReverseSentenceInConstantSpace {
+
+	public static void main(String[] args) 
+	{
+		String s = "I wish you merry christmas";
+		reverseSentenceInPlace(s);
+
+	}
+	
+	
+	private static void reverseSentenceInPlace(String givenString)
+	{
+	
+		StringBuffer s = new StringBuffer(givenString);
+	}
+
+}
