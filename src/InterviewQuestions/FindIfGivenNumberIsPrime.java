0b78285641740199c4b215f74ddbad820a911136
ninomeister <rajesh.thum@gmail.com>
2015-03-18 15:53:51 -0700

    fkjajnf
    akmfkadmf

--- src/InterviewQuestions/FindIfGivenNumberIsPrime.java
+++ src/InterviewQuestions/FindIfGivenNumberIsPrime.java
@@ -4,8 +4,23 @@ public class FindIfGivenNumberIsPrime {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		System.out.println(isPrime(50));
 
+	}
+	
+	private static boolean isPrime(int given)
+	{
+		boolean result = true;
+		
+		for(int i = 2; i < given; i++)
+		{
+			if(given%i == 0)
+				result = false;
+			    break;
+			    
+		}
+		
+		return result;
 	}
 
 }
