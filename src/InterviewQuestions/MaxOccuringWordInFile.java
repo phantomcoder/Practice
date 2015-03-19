efce18779e1b67f9d74bbf72364693f9aa2be14f
ninomeister <rajesh.thum@gmail.com>
2015-03-19 16:16:43 -0700

    zsada
    adasd

--- src/InterviewQuestions/MaxOccuringWordInFile.java
+++ src/InterviewQuestions/MaxOccuringWordInFile.java
@@ -1,11 +1,49 @@
 package InterviewQuestions;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MaxOccuringWordInFile {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
-		// TODO Auto-generated method stub
-
+		File file = new File("");
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String inputLine = null;
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		
+		try
+		{
+			while((inputLine = reader.readLine()) != null)
+			{
+				String[] words = inputLine.split("[\n\t\r.,::!?(){}]");
+				
+				for(String word : words)
+				{
+					if(map.get(word.toLowerCase()) == null)
+							{
+						map.put(word.toLowerCase(), 1);
+							}
+					else
+					{
+						int value = map.get(word.toLowerCase()).intValue();
+						value++;
+						map.put(word.toLowerCase(), value);
+					}
+				}
+			}
+		}
+		catch(Exception ex)
+		{
+			
+		}
+		finally
+		{
+			reader.close();
+		}
 	}
-
 }
